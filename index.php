20367d135c1149cce9d69d131729feebb7215ca1
ZaiDie <onok.asem@gmail.com>
2018-12-18 09:53:44 +0700

    login

--- index.php
+++ index.php
@@ -1,76 +1,50 @@
-<?php 
-include 'header.php';
-include 'admin/config/koneksi.php';
-include 'fungsi_paging.php';
-?>
-    <!-- Main Content -->
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-			
-						<?php
-							$p = new Paging();
-							$batas = 5;
-							$posisi = $p->cariPosisi($batas);
-							
-							$berita = mysqli_query($koneksi,"SELECT * FROM berita order by tanggal desc limit $posisi,$batas");
-							$res = mysqli_query($koneksi,"SELECT * FROM berita");
-							$set = mysqli_query($koneksi,"SELECT * FROM berita order by tanggal");
-							$rows = mysqli_num_rows($set);
-							if(mysqli_num_rows($res)){
-							while($data=mysqli_fetch_array($berita)){
-								$isi = substr($data['isi'],0,250);
-								$isi = substr($data['isi'],0,strrpos($isi," "));
-							$idse = $data['id_berita'];
-							
-						?>
-				<div class="col-sm-3 loc_1">
-			    	<a href=""><?php if($data['gambar_berita']!="") ?>
-					<img src="assets/img//<?= $data['gambar_berita']; ?>" class="img-responsive" /></a>
-			    </div>
-						<div class="col-sm-9">
-							<div class="post-preview">
-							<a href='berita_detail.php?id=<?=$idse?>'>
-								<h3 class="post-title">
-									<?= $data['judul']; ?> 
-								</h3>
-							</a>
-								<div class="post-preview">
-									<p class="post-meta">diposting pada <?= $data['tanggal']; ?></p>
-								</div>
-							</div>
-						</div>
-					<p align="justify">
-						<span class="m_2"><?= $isi; ?> ...</span>
-						<br>
-			    	 <a href="berita_detail.php?id=<?=$idse?>" >
-					 <span class="icon_text">Lihat selengkapnya</span>
-					 </a>
-					</p>		
-						<hr>		
-						
-						<?php 
-							} //tutup while
-						?>
-				 
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="center">
-					<?php
-						$jumlahData = mysqli_num_rows($res);
-						$jml_halaman = $p->jmlHalaman($jumlahData,$batas);
-						$link = $p->linkHal(@$_GET['halaman'],$jml_halaman);
-				
-						echo"<center><br \>$link</center>";
-					}
-					?>
-					
-                    </li>
-                </ul>
-            </div>			
-        </div>
-		
-    </div>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Administrator</title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../assets/js/jquery-ui/jquery-ui.css">
+	<script type="text/javascript" src="../assets/js/jquery.js"></script>
+	<script type="text/javascript" src="../assets/js/bootstrap.js"></script>
+	<script type="text/javascript" src="../assets/js/jquery-ui/jquery-ui.js"></script>
+	<?php include 'koneksi.php'; ?>
+	<style type="text/css">
+	.kotak{	
+		margin-top: 150px;
+	}
 
-    <hr>
-<?php include 'footer.php';?>
+	.kotak .input-group{
+		margin-bottom: 20px;
+	}
+	</style>
+</head>
+<body>	
+	<div class="container">
+		<?php 
+		if(isset($_GET['pesan'])){
+			if($_GET['pesan'] == "gagal"){
+				echo "<div style='margin-bottom:-55px' class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-warning-sign'></span>  Login Gagal !! Username dan Password Salah !!</div>";
+			}
+		}
+		?>
+		<div class="panel panel-default">
+			<form action="login_act.php" method="post">
+				<div class="col-md-4 col-md-offset-4 kotak">
+					<h3>Silahkan Login Admin ...</h3>
+					<div class="input-group">
+						<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+						<input type="text" class="form-control" placeholder="Username" name="username">
+					</div>
+					<div class="input-group">
+						<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+						<input type="password" class="form-control" placeholder="Password" name="password">
+					</div>
+					<div class="input-group">			
+						<input type="submit" class="btn btn-primary" value="Login">
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
